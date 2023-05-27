6935e407f5e94ca2e3af2900bfaa45e0dea44f32
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-05-27 13:50:34 -0300

    feat: add firs connection + test functions

--- src/server.js
+++ src/server.js
@@ -1,1 +1,33 @@
-console.log('teste')
+import { Client } from 'tmi.js';
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new Client({
+    connection: {
+        reconnect: true
+    },
+    channels: [ 'assistindinho' ],
+    identity: {
+		username: process.env.TWITCH_BOT_NAME,
+        password: process.env.OAUTH_TOKEN
+	},
+},);
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+    const isNotBot = tags.username.toLocaleLowerCase() !== process.env.TWITCH_BOT_NAME
+    
+    // if (isNotBot) {
+    //     client.say(channel, `Message : "${message}" was send to ${tags.username}`)
+    // }
+    if(message.toLowerCase() === '!hi') {
+		// "@alca, heya!"
+		client.say(channel, `@${tags.username}, heya!`);
+    }
+    if (tags.username === 'jota_apenas' && message.includes('salve')){
+        client.say(channel, `@${tags.username}, salve meu patrão!`)
+    }
+	console.log(`${tags['display-name']}: ${message}`);
+});
+		
