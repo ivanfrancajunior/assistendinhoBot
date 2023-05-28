221e83224fe55f112f05e9f5f62bde85f2971fe7
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-05-28 02:36:09 -0300

    feat: adicionados comandos iniciais

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import { Client } from "tmi.js";
+import fetch from "node-fetch"; // Importe a biblioteca "node-fetch" para fazer a requisição HTTP
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -12,75 +13,106 @@ const client = new Client({
     password: process.env.OAUTH_TOKEN,
   },
 });
+
 client.connect();
-let value = '';
-async function setMovie(value) {
-    if (value === null || '') return;
 
-    let movie = value;
-    
-    const buscarFilme = async (movie) => {
-    
-      const movieApiKey = process.env.OMDB_API_KEY;
-      const url = `http://www.omdbapi.com/?apikey=${movieApiKey}&t=${movie}&plot=full`;
-    
-      try {
-        const res = await fetch(url);
-        const data = await res.json();
-        
-        const movieInfo = {
-          title: data.Title,
-          sinopse: data.Plot,
-          runtime: data.Runtime,
-          year: data.Year,
-          director: data.Year,
-          rate: data.imdbRating,
-          year: data.Year,
-        };
-        console.log(movieInfo);
-    } catch (error) {
-        console.log("erro", error.message);
-      }
+let movieInfo = {}; // Variável global para armazenar as informações do filme
+
+const buscarFilme = async (movie) => {
+  const movieApiKey = process.env.OMDB_API_KEY;
+  const url = `http://www.omdbapi.com/?apikey=${movieApiKey}&t=${movie}&plot=full`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    movieInfo = {
+      title: data.Title,
+      sinopse: data.Plot,
+      runtime: data.Runtime,
+      year: data.Year,
+      director: data.Director,
+      rate: data.imdbRating,
     };
-      
-    buscarFilme(movie)
-  
-}
-setMovie("thor")
 
+    console.log(movieInfo);
+  } catch (error) {
+    console.log("Erro:", error.message);
+  }
+};
 
 client.on("message", (channel, tags, message, self) => {
-  const isNotBot =
-    tags.username?.toLocaleLowerCase() !== process.env.TWITCH_BOT_NAME;
+  const isNotBot = tags.username?.toLowerCase() !== process.env.TWITCH_BOT_NAME;
 
+  //Adicionar um filme
+  if (isNotBot && message.toLowerCase().startsWith("!setfilme ")) {
+    const movie = message.slice(10); // Remove o prefixo "!setfilme "
+
+    console.log(movie);
+
+    buscarFilme(movie);
+
+    if (movie !== "" || null) {
+      client.say(channel, `O filme "${movie}" foi adicionado por ${tags.username}`);
+    } else {
+      client.say(channel, `Ocorreu um erro ao adicionar o filme, @${tags.username}`);
+    }
+  }
+
+  
+  // Comandos 
+  const showMovie = () => {
+    if (!movieInfo.title) {
+      return client.say(channel, "Você ainda não adicionou um filme.");
+    } else {
+      return client.say(channel, `Estamos assitindo o filme "${movieInfo.title}", @${tags.username} -  Sinopse: ${movieInfo.sinopse} - Duração: ${movieInfo.runtime}`);
+    }
+  };
+
+  const showSinopse = () => {
+    if (!movieInfo.title) {
+      return client.say(channel, "Você ainda não adicionou um filme.");
+    } else {
+      return client.say(channel, `Sinopse: ${movieInfo.sinopse}`);
+    }
+  };
+
+  const showDuracao = () => {
+    if (!movieInfo.title) {
+      return client.say(channel, "Você ainda não adicionou um filme.");
+    } else {
+      return client.say(channel, `Duração: ${movieInfo.runtime}`);
+    }
+  };
+  const showRating = () => {
+    if (!movieInfo.title) {
+      return client.say(channel, "Você ainda não adicionou um filme.");
+    } else {
+      return client.say(channel, `Segundo a IMDB o filme teve ${movieInfo.rate} de nota.`);
+    }
+  };
+
+ // Chat spec
   if (isNotBot && message.toLowerCase() === "oi") {
-    client.say(channel, `Olá @${tags.username} :D ${console.log(Date.now())}`);
-  }
-  if (isNotBot && message.toLowerCase() === "!hi") {
-    // "@alca, heya!"
-    client.say(channel, `@${tags.username}, heya!`);
-  }
-  if (tags.username === "jota_apenas" && message.includes("salve")) {
-    client.say(channel, `@${tags.username}, salve meu patrão!`);
+    client.say(channel, `Olá @${tags.username} :D`);
   }
 
   if (isNotBot && message.toLowerCase().includes("@assistindinho,")) {
-    client.say(channel, `Oi fala comigo @${tags.username} `);
+    client.say(channel, `Oi, fala comigo @${tags.username}`);
   }
+
   if (message.toLowerCase() === "!sinopse") {
-    //TODO
-    client.say(channel, `alguma sinopse... `);
+    showSinopse();
   }
+
   if (message.toLowerCase() === "!duração") {
-    //TODO
-    client.say(channel, `alguma duração `);
+    showDuracao();
   }
-  if (message.toLowerCase() === "!tempo") {
-    //TODO
-    client.say(channel, `algum tempo `);
+  if (message.toLowerCase() === "!filme") {
+    showMovie();
   }
-  if (message.toLowerCase() === "!inicio") {
-    //TODO
-    client.say(channel, `algum inicio `);
+  if (message.toLowerCase() === "!imdb") {
+    showRating();
   }
 });
+
